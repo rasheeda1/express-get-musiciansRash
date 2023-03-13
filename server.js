e71e5797de5e8302afeff1f9c732ce0ddab6d3dc
rasheedarahimi <rasheedarahimi10@outlook.com>
2023-03-13 14:11:50 +0000

    initial commit

--- server.js
+++ server.js
@@ -6,8 +6,28 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
+app.get('/musicians', async(req, res) => {
+    try{
+        const musicians = await Musician.findAll();
+        res.json(musicians);
+    }catch (error){
+        res.status(500).send('Error')
+    }
+})
+
+app.get('/musicians', async (req, res) => {
+    try{
+        const musicians = await Musician.findByPk(req.params.id);
+    if (!musicians){
+        return req.status('200').json({ error: 'No musician found'});
+    }
+    res.json(musicians);
+} catch (error) {
+    res.status('500').json({error: 'Error'});
+}
+})
 
 app.listen(port, () => {
     sequelize.sync();
-    console.log(`Listening on port ${port}`)
+    console.log(`Listening on http://localhost:${port}`)
 })
